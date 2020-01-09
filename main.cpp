2546e5553e47d343d0f3250d7e6f3b6b56acb166
Devin Jaenicke <djaenicke.92@gmail.com>
2020-01-08 23:27:00 -0500

    Replaced DC_Motor class with L298 class
    The K64F really interacts with the motor driver, and not the motors. Therefore, it makes more sense to have a motor driver object.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "mbed.h"
-#include "dc_motor.h"
+#include "l298.h"
 #include "encoder.h"
 #include "io_abstraction.h"
 #include "battery_monitor.h"
+
+/* Redefine motors ids to locations for code readability */
+#define L_MOTOR l298::MOTOR_A
+#define R_MOTOR l298::MOTOR_B
 
 #define PULSES_PER_REV   (192)
 
@@ -10,16 +14,16 @@ static DigitalOut init_complete_led(LED_BLUE);
 static Serial debug_out(USBTX, USBRX, 115200);
 
 /* The following objects are here temporarily for testing */
-static dc_motor::DC_Motor r_motor(dc_motor::RIGHT_SIDE);
-static dc_motor::DC_Motor l_motor(dc_motor::LEFT_SIDE);
+static l298::L298 motor_driver(MOTOR_ENA, MOTOR_ENB, MOTOR_IN1, 
+                               MOTOR_IN2, MOTOR_IN3, MOTOR_IN4);
 
 static encoder::Encoder r_encoder(R_ENCODER, PullUp, PULSES_PER_REV, 50);
 static encoder::Encoder l_encoder(L_ENCODER, PullUp, PULSES_PER_REV, 50);
 
 static Timeout print_timeout;
 static volatile bool print_speeds;
-static float r_wheel_speed;
-static float l_wheel_speed;
+static float r_wheel_ang_v;
+static float l_wheel_ang_v;
 
 void set_print_speeds(void);
 
@@ -34,21 +38,21 @@ int main() {
   debug_out.printf("Battery voltage = %.2f\r\n", ReadBatteryVoltage());
   init_complete_led.write(0);
 
-  r_motor.SetDirection(dc_motor::FORWARD);
-  r_motor.SetDC(50);
+  motor_driver.SetDirection(L_MOTOR, l298::REVERSE);
+  motor_driver.SetDirection(R_MOTOR, l298::FORWARD);
 
-  l_motor.SetDirection(dc_motor::FORWARD);
-  l_motor.SetDC(50);
+  motor_driver.SetDC(L_MOTOR, 75);
+  motor_driver.SetDC(R_MOTOR, 75);
 
   print_timeout.attach(&set_print_speeds, 2.0);
 
   while (true) {
-    r_wheel_speed = r_encoder.GetWheelSpeed();
-    l_wheel_speed = l_encoder.GetWheelSpeed();
+    r_wheel_ang_v = r_encoder.GetWheelSpeed() * motor_driver.GetDirection(R_MOTOR);
+    l_wheel_ang_v = l_encoder.GetWheelSpeed() * motor_driver.GetDirection(L_MOTOR);
 
     if (print_speeds) {
-      debug_out.printf("\r\nRight wheel speed = %.2f\r\n", r_wheel_speed);
-      debug_out.printf("Left wheel speed = %.2f\r\n", l_wheel_speed);
+      debug_out.printf("\r\nRight wheel angular velocity = %.2f\r\n", r_wheel_ang_v);
+      debug_out.printf("Left angular velocity = %.2f\r\n", l_wheel_ang_v);
       print_speeds = false;
     }
 
