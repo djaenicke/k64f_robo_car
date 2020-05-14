8c7ba9e86c542565f76f4a8c4c7f227039f26e41
Devin Jaenicke <djaenicke.92@gmail.com>
2020-05-14 15:40:07 -0400

    Deleting now unused code

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #include "io_abstraction.h"
 #include "battery_monitor.h"
 #include "motor_controls.h"
-#include "serial_ctrl.h"
-#include "inertial_data_msg.h"
 
 #include "mpu6050.h"
 #include "fxos8700.h"
@@ -29,10 +27,11 @@ static Thread motor_controls_thread(osPriorityRealtime);
 static mpu6050::MPU6050 imu1(I2C_SDA, I2C_SCL);
 static fxos8700::FXOS8700 imu2(I2C_SDA, I2C_SCL);
 
-static Inertial_Data_Msg_T Inertial_Data_Msg;
-
-static void PopulateImuMsgs(void);
-static void LogInertialData(void);
+Wheel_Ang_V_T wheel_speed_sp;
+Wheel_Ang_V_T wheel_speed_fb;
+mpu6050::Accel_Data_T mpu_accel_data;
+mpu6050::Gyro_Data_T  mpu_gyro_data;
+fxos8700::Sensor_Data_T fxos_data;
 
 // main() runs in its own thread in the OS
 int main() {
@@ -44,7 +43,6 @@ int main() {
   blue_led.write(1);
 
   InitMotorControls();
-  InitSerialCtrl();
 
   /* Wait 2 seconds before calibrating the IMUs so 
      the user doesn't affect the calibration process by touching the robot*/
@@ -70,46 +68,23 @@ int main() {
   while (true) {
     t_start = t.read_ms();
 
+    GetWheelAngVSp(&wheel_speed_sp); /* Setpoint */
+    GetWheelAngV(&wheel_speed_fb);   /* Feedback */
+
+    imu2.ReadData(&fxos_data);
+    //fxos_ax = -1.0f * fxos_data.ay;
+    //fxos_ay = fxos_data.ax;
+
+    imu1.ReadAccelData(&mpu_accel_data);
+    //mpu_ax = -1.0f * mpu_accel_data.ax;
+    //mpu_ay = -1.0f * mpu_accel_data.ay;
+
+    imu1.ReadGyroData(&mpu_gyro_data);
+
 #if ROS_ENABLED
     nh.getHardware()->get_recv_data();
     nh.spinOnce();
 #endif
-#if USE_XBEE
-    XbeeProcessRxData();
-    LogInertialData();
-#endif
     ThisThread::sleep_for(50-(t.read_ms()-t_start));
   }
-}
-
-void LogInertialData(void) {
-  Wheel_Ang_V_T speeds;
-  mpu6050::Accel_Data_T mpu_accel_data;
-  mpu6050::Gyro_Data_T  mpu_gyro_data;
-  fxos8700::Sensor_Data_T fxos_data;
-
-  Inertial_Data_Msg.ts = t.read_us();
-
-  GetWheelAngVSp(&speeds); /* Setpoint */
-  Inertial_Data_Msg.l_speed_sp = ENCODE_SIGNAL(speeds.l);
-  Inertial_Data_Msg.r_speed_sp = ENCODE_SIGNAL(speeds.r);
-
-  GetWheelAngV(&speeds); /* Feedback */
-  Inertial_Data_Msg.l_speed_fb = ENCODE_SIGNAL(speeds.l);
-  Inertial_Data_Msg.r_speed_fb = ENCODE_SIGNAL(speeds.r);
-
-  imu2.ReadData(&fxos_data);
-  Inertial_Data_Msg.fxos_ax = ENCODE_SIGNAL(-1.0f * fxos_data.ay);
-  Inertial_Data_Msg.fxos_ay = ENCODE_SIGNAL(fxos_data.ax);
-  Inertial_Data_Msg.fxos_az = ENCODE_SIGNAL(fxos_data.az);
-
-  imu1.ReadAccelData(&mpu_accel_data);
-  Inertial_Data_Msg.mpu_ax = ENCODE_SIGNAL(-1.0f * mpu_accel_data.ax);
-  Inertial_Data_Msg.mpu_ay = ENCODE_SIGNAL(-1.0f * mpu_accel_data.ay);
-  Inertial_Data_Msg.mpu_az = ENCODE_SIGNAL(mpu_accel_data.az);
-
-  imu1.ReadGyroData(&mpu_gyro_data);
-  Inertial_Data_Msg.mpu_gz = ENCODE_SIGNAL(mpu_gyro_data.gz);
-
-  XbeeTxData((char*)(&Inertial_Data_Msg), sizeof(Inertial_Data_Msg));
 }
